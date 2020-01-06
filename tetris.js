0bcc787ba348e70f1da5281b394534e61c533d39
Nick Bansal <extreme_wired@msn.com>
2020-01-06 16:00:46 +0000

    created an offset

--- tetris.js
+++ tetris.js
@@ -8,14 +8,18 @@ context.fillStyle = '#FEC952';
 context.fillRect(0, 0, width, height);
 
 const matrix = [
-  [0, 0, 0], [1, 1, 1], [0, 1, 0],
+    [0, 0, 0], [1, 1, 1], [0, 1, 0],
 ];
 
-matrix.forEach((row, y) => {
-  row.forEach((value, x) => {
-    if (value !== 0) {
-      context.fillStyle = '#C33C23';
-      context.fillRect(x, y, 1, 1);
-    }
-  });
-});
+const drawMatrix = (shape, offset) => {
+    shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = '#C33C23';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
+    });
+};
+
+drawMatrix(matrix, { x: 5, y: 5 });
